1901f3248440a5d53829e28977286850549d4989
z-zha <2635067653@qq.com>
2020-07-22 00:10:10 +0800

    提交

--- myNewProjects/myblog/demo/src/views/myself.vue
+++ myNewProjects/myblog/demo/src/views/myself.vue
@@ -43,9 +43,9 @@
 
             <div class="aboutMe">
               <a-descriptions
-                bordered="true"
+                bordered
                 :column="{ xxl: 2, xl: 2, lg: 2, md: 2, sm: 2, xs: 1 }"
-                size="middle"
+                
               >
                 <a-descriptions-item label="姓名">查佳慧</a-descriptions-item>
                 <a-descriptions-item label="性别">女</a-descriptions-item>
@@ -58,12 +58,10 @@
                 <a-descriptions-item label="联系方式">187-7925-1461</a-descriptions-item>
                 <a-descriptions-item label="电子邮箱">2635067653@qq.com</a-descriptions-item>
                 <a-descriptions-item label="技术栈">
-                  技术栈
-                  <br />Database version: 3.4
-                  <br />Package: dds.mongo.mid
-                  <br />Storage space: 10 GB
-                  <br />Replication factor: 3
-                  <br />Region: East China 1
+                  <span>前端：HTML/CSS，JavaScript， vue，mpvue， 微信小程序</span><br />
+                  <span>后端：Koa2，egg.js，node</span><br />
+                  <span>数据库： mysql</span><br />
+                  <span>工具： git,，postman，Easy mock</span><br />
                 </a-descriptions-item>
               </a-descriptions>
             </div>
@@ -143,6 +141,9 @@ export default {
   margin-right: 300px;
   margin-bottom: 50px;
   // text-align: left;
+  span {
+    line-height: 40px;
+  }
 }
 /* .back span, .web span {
   c
